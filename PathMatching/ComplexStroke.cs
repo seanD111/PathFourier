3f49ec58462cf048dd41da6e2be5cfbda13573b7
seanD111 <sean_delong@hotmail.ca>
2020-02-03 02:56:20 -0500

    first version of DFT added

---
+++ PathMatching/ComplexStroke.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Numerics;
+
+
+namespace PathMatching
+{
+    public class Coefficient
+    {
+        public Complex negative;
+        public Complex positive;
+    }
+
+
+    class ComplexStroke 
+    {
+        private Stroke _BaseStroke;
+
+        public Stroke BaseStroke {
+            get
+            {
+                return _BaseStroke;
+            }
+            set
+            {
+                _BaseStroke = value;
+                UpdateComplexPoints();
+            }
+        }
+
+
+        public List<Complex> ComplexPoints { get; set; }
+        public List<Coefficient> DFTCoefficients { get; set; }
+
+        //private Stroke _reconstructedStroke { get; set; }
+
+        public ComplexStroke(StylusPointCollection stylusPoints)
+        {
+            BaseStroke = new Stroke(stylusPoints);
+            UpdateComplexPoints();
+        }
+
+        public ComplexStroke(Stroke stroke)
+        {
+            BaseStroke = stroke.Clone();
+            UpdateComplexPoints();
+        }
+
+
+
+        private void UpdateComplexPoints()
+        {
+            ComplexPoints = new List<Complex>();
+            foreach (StylusPoint point in BaseStroke.StylusPoints)
+            {
+                Complex cPoint = new Complex(point.X, point.Y);
+                ComplexPoints.Add(cPoint);
+            }
+            DFTCoefficients = DFT(ComplexPoints);
+           
+        }
+
+
+        public Stroke ReconstructedStroke(int num = -1)
+        {
+            int coefCount = (-1 < num && num < DFTCoefficients.Count) ? num : DFTCoefficients.Count;
+            return IDFT(DFTCoefficients, coefCount);
+        }
+
+        public static List<Coefficient> DFT(List<Complex> complices)
+        {
+            int N = complices.Count;
+            List<Coefficient> coeffs = new List<Coefficient>();
+            
+            for (int k = 0; k < N; k++)
+            {
+
+                Complex pos = new Complex(0, 0);
+                Complex neg = new Complex(0, 0);
+
+                for (int m = 0; m<N; m++)
+                {
+                    Complex a = -1.0 * Complex.ImaginaryOne*2.0 * Math.PI * m * k * ((double)1 / (double)N);
+
+                    pos = pos + complices[m] * Complex.Exp(a);
+                    neg = neg + complices[m] * Complex.Exp(-a);
+                }
+                Coefficient coeff = new Coefficient
+                {
+                    positive = pos / N,
+                    negative = neg / N
+                };
+
+                coeffs.Add(coeff);
+            }
+            return coeffs;
+        }
+
+        public static Stroke IDFT(List<Coefficient> coeffs, int M)
+        {
+            StylusPointCollection points = new StylusPointCollection(M);
+
+            int N = coeffs.Count;
+            for (int m = 0; m < N; m++)
+            {
+
+                Complex point = new Complex(0,0);
+                for (int k = -M/2; k <= M/2; k++)
+                {
+                    Complex coeff = k < 0 ? coeffs[-k].negative : coeffs[k].positive;
+
+                    Complex a = Complex.ImaginaryOne * 2.0 * Math.PI * m * k * ((double)1 / (double)N);
+                    point += coeff * Complex.Exp(a);
+                    
+                }
+
+                points.Add(new StylusPoint(point.Real, point.Imaginary));
+            }
+
+            return new Stroke(points);
+        }
+
+
+    }
+}
--- PathMatching/PathMatcher.xaml.cs
+++ PathMatching/PathMatcher.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Ink;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.ComponentModel;
 
 namespace PathMatching
 {
@@ -23,12 +26,116 @@ namespace PathMatching
         public PathMatcher()
         {
             InitializeComponent();
+          
+        }
+
+        private void strokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            if (sender == inkIn1){
+                int sum = 0;
+                 foreach (Stroke stroke in inkIn1.Strokes)
+                {
+                    sum += stroke.StylusPoints.Count;
+                }
+                coefficientSlider1.Maximum = sum;
+            }
+            else if (sender == inkIn2)
+            {
+                int sum = 0;
+                foreach (Stroke stroke in inkIn2.Strokes)
+                {
+                    sum += stroke.StylusPoints.Count;
+                }
+                coefficientSlider2.Maximum = sum;
+            }     
+        }
+
+        private void clearIn1(object sender, RoutedEventArgs e)
+        {
+            inkIn1.Strokes = new StrokeCollection();
+            inkReconstruction1.Strokes = new StrokeCollection();
+
+        }
+
+        private void clearIn2(object sender, RoutedEventArgs e)
+        {
+            inkIn2.Strokes = new StrokeCollection();
+            inkReconstruction2.Strokes = new StrokeCollection();
+        }
+
+        private void strokeCollected(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void cSlide1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            StylusPointCollection allPoints = new StylusPointCollection();
+
+            foreach (Stroke stroke in inkIn1.Strokes)
+            {
+                allPoints.Add(stroke.StylusPoints);
+            }
+            
+            ComplexStroke cStroke = new ComplexStroke(allPoints);
+            
+
+            inkReconstruction1.Strokes = new StrokeCollection();
+            inkReconstruction1.Strokes.Add(cStroke.ReconstructedStroke((int)coefficientSlider1.Value));
+
+        }
+
+        private void cSlide2(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            StylusPointCollection allPoints = new StylusPointCollection();
+
+            foreach (Stroke stroke in inkIn2.Strokes)
+            {
+                allPoints.Add(stroke.StylusPoints);
+            }
+
+            ComplexStroke cStroke = new ComplexStroke(allPoints);
+
+
+            inkReconstruction2.Strokes = new StrokeCollection();
+            inkReconstruction2.Strokes.Add(cStroke.ReconstructedStroke((int)coefficientSlider2.Value));
+
         }
     }
 
 
-    public class Coefficients //implement INotifyPropertyChanged to get the power of binding :)
+    public class MyData : INotifyPropertyChanged
     {
-        public int Count { get; set; }
+        private int coefficientCount;
+        private int takeMinimum;
+
+        public MyData() { }
+
+        public int TakeMinimum
+        {
+            get { return takeMinimum; }
+            set
+            {
+                takeMinimum = value;
+                OnPropertyChanged("TakeMinimum");
+            }
+        }
+
+        public int CoefficientCount
+        {
+            get { return coefficientCount; }
+            set
+            {
+                coefficientCount = value;
+                OnPropertyChanged("CoefficientCount");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
     }
 }
